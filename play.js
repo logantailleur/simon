08a04bcbd8b436b8b271ae622fbcb6a1dea1db32
logantailleur <logantailleur@gmail.com>
2023-03-04 11:50:02 -0700

    Finalize code

--- play.js
+++ play.js
@@ -24,7 +24,6 @@ class Button {
     this.paint(25);
   }
 
-  // Work around Safari's rule to only play sounds if given permission.
   async play(volume = 1.0) {
     this.sound.volume = volume;
     await new Promise((resolve) => {
